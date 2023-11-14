199a9e05944817b1760d77943f4af4d6f35b78ea
nodirbek984 <nodirbeknegmatov2008>
2023-11-14 15:04:07 +0500

    dars

---
+++ main.js
@@ -0,0 +1,125 @@
+//if1
+//let son = prompt("sonni kiriting")
+//if (son > 0){
+// son++
+//  alert(son);
+//}
+
+//if2
+// let son = prompt("sonni kiriting")
+// if (son > 0){
+//   son++
+//   alert(son)
+// }else{
+//   son--
+//   son--
+//   alert(son)
+// }
+
+//if3
+// let son = +prompt("sonni kiriting");
+// if (son > 0) {
+//   son++;
+//   alert(son);
+// } else if (son == 0) {
+//   alert(son + 10);
+// } else {
+//   son--;
+//   son--;
+//   alert(son);
+// }
+
+//if4
+// let son1 = +prompt("1-son");
+// let son2 = +prompt("2-son");
+// let son3 = +prompt("3-son");
+// if (son1 > 0 && son2 > 0 && son3 > 0) {
+//   alert("3 ta musbat");
+// } else if (son1 < 0 && son2 > 0 && son3 > 0 ) {
+//   alert("2 ta son musbat");
+// } else if (son1 < 0 && son2 < 0 && son3 > 0) {
+//   alert("1 ta musbat");
+// } else if (son1 < 0 && son2 < 0 && son3 < 0) {
+//   alert("0 ta musbat");
+// }
+
+//if5
+// let a = +prompt("son1");
+// let b = +prompt("son2");
+// let d = +prompt("son3");
+// if (a < 0 && b < 0 && c < 0) {
+//   alert("3 ta musbat, 0 ta manfiy");
+// } else if (
+//   (a > 0 && b < 0 && c > 0) ||
+//   (a < 0 && b > 0 && c < 0) ||
+//   (a < 0 && b < 0 && c > 0)
+// ) {
+//   alert("2 ta musbat, 1 ta manfiy");
+// } else if (
+//   (a > 0 && b > 0 && c < 0) ||
+//   (a < 0 && b > 0 && c > 0) ||
+//   (a > 0 && b < 0 && c > 0)
+// ) {
+//   alert("1 ta musbat, 2 ta manfiy");
+// } else if (a > 0 && b > 0 && c > 0) {
+//   alert("0 ta musbat, 3 ta manfiy");
+// }
+
+//if6
+// let a = +prompt("1-son");
+// let b = +prompt("2-son");
+// if (a > b) {
+//   alert("1-son katta");
+// } else {
+//   alert("2-son katta");
+// }
+
+//if7
+// let a = +prompt("1-son");
+// let b = +prompt("2-son");
+// if (a > b) {
+//   alert(b && a);
+// } else {
+//   alert(a && b);
+// }
+
+//if8
+// let a = +prompt("1-son");
+// let b = +prompt("2-son");
+// if (a > b) {
+//   alert("1 son katta");
+// } else {
+//   alert("2-son katta");
+// }
+
+//boolean1
+// let a = +prompt("sonni liriting");
+// if (a > 0) {
+//   alert("musbat");
+// }
+
+//boolean2
+// let a = +prompt("sonni liriting");
+// if (a % 2 == 1){
+//     alert("toq")
+// }
+
+//boolean3
+// let a = +prompt("sonni liriting");
+// if (a % 2 == 0){
+//     alert("juft")
+// }
+
+//boolean4
+// let a = +prompt("1-son");
+// let b = +prompt("2-son");
+// if (a > 2 && b <= 3){
+// alert('togri')
+// }
+
+//boolean5
+// let a = +prompt("1-son");
+// let b = +prompt("2-son");
+// if (a >= 0 || b < -2){
+//     alert('togri')
+// }
